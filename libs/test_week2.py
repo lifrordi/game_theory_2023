757a58d7ab00bd6d0a75fd84afbeb4d179054a21
schmid-equilibre <schmid@equilibretechnologies.com>
2023-10-22 18:45:43 +0200

    Bugix

--- libs/test_week2.py
+++ libs/test_week2.py
@@ -4,12 +4,12 @@ import pytest
 import numpy.testing as nptest
 
 
-def test_week1():
+def test_week2():
     matrix_p1 = np.array([[0, 0, -10], [1, -10, -10], [-10, -10, -10]])
     matrix_p2 = np.array([[0, 1, -10], [0, -10, -10], [-10, -10, -10]])
 
 
-    result = week2.verify_support_one_side(matrix = matrix_p1, support_row=[0, 1], support_col = [0, 1, 2])
+    result = week2.verify_support_one_side(matrix = matrix_p1, support_row=[0, 1], support_col = [0, 1])
     nptest.assert_allclose(result, [0.90909, 0.09090], rtol=1e-3)
 
 
